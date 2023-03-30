30885a1f14b6c3231e513077a20ed6dd8bac57c8
Harvey <harvey.cheng@intel.com>
2023-03-30 11:32:21 -0700

    add random search to view

---
+++ libsigopt/compute/views/rest/random_search_next_points.py
@@ -0,0 +1,21 @@
+# Copyright © 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
+from libsigopt.compute.views.view import View
+
+class RandomSearchNextPoints(View):
+  view_name = "random_search_next_points"
+
+  def view(self):
+    num_to_sample = self.params["num_to_sample"]
+    if self.domain.priors and not self.domain.constraint:
+      categorical_next_points = self.domain.generate_random_points_according_to_priors(num_to_sample)
+    else:
+      categorical_next_points = self.domain.generate_quasi_random_points_in_domain(num_to_sample)
+
+    results = {
+      "endpoint": self.view_name,
+      "points_to_sample": categorical_next_points,
+      "tag": self.tag,
+    }
+    return results

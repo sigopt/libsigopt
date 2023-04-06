9a5969dd478efd7c3b228cb9e1ac49efcecfd225
malkomes <74630871+malkomes@users.noreply.github.com>
2023-04-06 12:59:44 -0300

    lint

--- libsigopt/aux/utils.py
+++ libsigopt/aux/utils.py
@@ -7,13 +7,10 @@ def is_integer(num):
     return False
   return isinstance(num, (int | numbers.Integral))
 
+
 def is_number(x):
   if isinstance(x, bool):
     return False
   if isinstance(x, float) and not numpy.isfinite(x):
     return False
   return isinstance(x, numbers.Number) or is_integer(x)
-
-
-
-

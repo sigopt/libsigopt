eb5d2a0e5624d2fcbcc70691921372bd2d73a6d4
malkomes <74630871+malkomes@users.noreply.github.com>
2023-04-11 11:05:07 -0500

    remove numpy from utils

--- libsigopt/aux/utils.py
+++ libsigopt/aux/utils.py
@@ -2,19 +2,19 @@
 #
 # SPDX-License-Identifier: Apache License 2.0
 import numbers
-
-import numpy
-
+import math
 
 def is_integer(num):
   if isinstance(num, bool):
     return False
   return isinstance(num, (int | numbers.Integral))
 
+def is_finite(x):
+  return not (math.isinf(x) or math.isnan(x))
 
 def is_number(x):
   if isinstance(x, bool):
     return False
-  if isinstance(x, float) and not numpy.isfinite(x):
+  if isinstance(x, float) and not is_finite(x):
     return False
   return isinstance(x, numbers.Number) or is_integer(x)
